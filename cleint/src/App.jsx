059b16e9db89cc66bd409bb9688bdee724bc1972
abdulwasay <abdulwasayhashmi983@gmail.com>
2024-01-10 15:29:04 +0500

    made ceint an server side made some pages in cleint and smple server

---
+++ cleint/src/App.jsx
@@ -0,0 +1,22 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './Pages/Home';
+import SignIn from './Pages/SignIn';
+import SignUp from './Pages/Signup';
+import About from './Pages/About';
+import Profile from './Pages/Profile';
+import Header from './Component/Header';
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/sign-in' element={<SignIn />} />
+        <Route path='/sign-up' element={<SignUp />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/profile' element={<Profile />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
