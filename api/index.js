8dda3b09ce56e87fed3af3be0fcf141a8ad12d66
abdulwasay <abdulwasayhashmi983@gmail.com>
2024-01-11 10:00:15 +0500

    created user route and controller seperatedly

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import mongoose from "mongoose"
 import dotenv from "dotenv"
+import router from "./routes/user.route.js";
 dotenv.config()
 mongoose.connect(process.env.MONGO_URL)
 .then(()=>{
@@ -13,3 +14,4 @@ const app = express();
 app.listen(3000, ()=>{
     console.log("server is running on 3000")
 })
+app.use("/api/user", router);
