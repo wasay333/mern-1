ed880a8287432311fea95ad77fb8c12379edd6fb
abdulwasay <abdulwasayhashmi983@gmail.com>
2024-01-12 22:27:27 +0500

    complete update listing functionality

--- api/Controller/listing.controller.js
+++ api/Controller/listing.controller.js
@@ -1,6 +1,5 @@
 import Listing from '../models/listing.model.js';
 import { errorHandler } from '../utils/error.js';
-
 export const createListing = async (req, res, next) => {
   try {
     const listing = await Listing.create(req.body);
@@ -24,7 +23,6 @@ export const deleteListing = async (req, res, next) => {
     next(error);
   }
 };
-
 export const updateListing = async (req, res, next) => {
   const listing = await Listing.findById(req.params.id);
   if (!listing) {
@@ -33,7 +31,6 @@ export const updateListing = async (req, res, next) => {
   if (req.user.id !== listing.userRef) {
     return next(errorHandler(401, 'You can only update your own listings!'));
   }
-
   try {
     const updatedListing = await Listing.findByIdAndUpdate(
       req.params.id,
@@ -45,3 +42,16 @@ export const updateListing = async (req, res, next) => {
     next(error);
   }
 };
+
+export const getListing = async (req, res, next) => {
+try {
+  const listing = await Listing.findById(req.params.id);
+  if (!listing) {
+    return next(errorHandler(404, 'Listing not found!'));
+  }
+  res.status(200).json(listing);
+} catch (error) {
+  next(error);
+}
+
+}
--- cleint/src/App.jsx
+++ cleint/src/App.jsx
@@ -3,10 +3,11 @@ import Home from './pages/Home';
 import SignIn from './pages/SignIn';
 import SignUp from './pages/Signup';
 import About from './pages/About';
-import Profile from './pages/Profile';
+import Profile from './Pages/Profile';
 import Header from './components/Header';
 import PrivateRoute from './components/PrivateRoute';
-import CreateListing from './Pages/CreateListing';
+import CreateListing from './pages/CreateListing';
+import UpdateListing from './pages/UpdateListing';
 
 export default function App() {
   return (
@@ -20,6 +21,10 @@ export default function App() {
         <Route element={<PrivateRoute />}>
           <Route path='/profile' element={<Profile />} />
           <Route path='/create-listing' element={<CreateListing />} />
+          <Route
+            path='/update-listing/:listingId'
+            element={<UpdateListing />}
+          />
         </Route>
       </Routes>
     </BrowserRouter>
