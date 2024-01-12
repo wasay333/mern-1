fe58ac939c14d45f03a0b5c20727635cef869993
abdulwasay <abdulwasayhashmi983@gmail.com>
2024-01-12 16:40:55 +0500

    add delete user functionality

--- api/Controller/user.controller.js
+++ api/Controller/user.controller.js
@@ -1,13 +1,11 @@
 import bcryptjs from 'bcryptjs';
 import User from '../models/user.model.js';
 import { errorHandler } from '../utils/error.js';
-
 export const test = (req, res) => {
   res.json({
     message: 'Api route is working!',
   });
 };
-
 export const updateUser = async (req, res, next) => {
   if (req.user.id !== req.params.id)
     return next(errorHandler(401, 'You can only update your own account!'));
@@ -15,7 +13,6 @@ export const updateUser = async (req, res, next) => {
     if (req.body.password) {
       req.body.password = bcryptjs.hashSync(req.body.password, 10);
     }
-
     const updatedUser = await User.findByIdAndUpdate(
       req.params.id,
       {
@@ -28,10 +25,20 @@ export const updateUser = async (req, res, next) => {
       },
       { new: true }
     );
-
     const { password, ...rest } = updatedUser._doc;
-
     res.status(200).json(rest);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const deleteUser = async (req, res, next) => {
+  if (req.user.id !== req.params.id)
+    return next(errorHandler(401, 'You can only delete your own account!'));
+  try {
+    await User.findByIdAndDelete(req.params.id);
+    res.clearCookie('access_token');
+    res.status(200).json('User has been deleted!');
   } catch (error) {
     next(error);
   }
