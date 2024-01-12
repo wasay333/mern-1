202cbe659d3cc7765419b91ee6da05e0ebe10b5b
abdulwasay <abdulwasayhashmi983@gmail.com>
2024-01-12 20:24:25 +0500

    add create listing api route

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRouter from './routes/user.route.js';
 import authRouter from './routes/auth.route.js';
+import listingRouter from './routes/listing.route.js';
 import cookieParser from 'cookie-parser';
 dotenv.config();
 
@@ -15,16 +16,16 @@ mongoose
     console.log(err);
   });
 const app = express();
-
 app.use(express.json());
-
 app.use(cookieParser());
-
 app.listen(3000, () => {
   console.log('Server is running on port 3000!');
 });
+
 app.use('/api/user', userRouter);
 app.use('/api/auth', authRouter);
+app.use('/api/listing', listingRouter);
+
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
   const message = err.message || 'Internal Server Error';
