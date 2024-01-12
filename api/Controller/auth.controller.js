7c902e2b6cc10d9f4b9444c68ef12532cc4dfc02
abdulwasay <abdulwasayhashmi983@gmail.com>
2024-01-12 14:51:21 +0500

    add middleware then created sign up page in cleint folder add created util folder in api folder for cuatom error

--- api/Controller/auth.controller.js
+++ api/Controller/auth.controller.js
@@ -1,6 +1,6 @@
 import User from "../Models/user.model.js"
 import bycryptjs from "bcrypt"
-export const Signup = async(req, res)=>{
+export const Signup = async(req, res, next)=>{
 const {username, email, password}= req.body;
 const hashedPassword = bycryptjs.hashSync(password, 10);
 const newUser = new User({username, email, password:hashedPassword});
@@ -8,6 +8,7 @@ try{
     await newUser.save();
     res.status(201).json('User created successfully!');
 }catch(error){
-res.status(500).json(error.message);
+// res.status(500).json(error.message);
+next(error);
 }
 }
