3c92eb1f4554e6590bde612fbc87b2d6c16abfbc
abdulwasay <abdulwasayhashmi983@gmail.com>
2024-01-12 16:26:16 +0500

    complete update user functionality

--- api/utils/verifyUser.js
+++ api/utils/verifyUser.js
@@ -1,11 +1,12 @@
-import express from 'express';
-import { test, updateUser } from '../controllers/user.controller.js';
-import { verifyToken } from '../utils/verifyUser.js';
+import jwt from 'jsonwebtoken';
+import { errorHandler } from './error.js';
 
-
-const router = express.Router();
-
-router.get('/test', test);
-router.post('/update/:id', verifyToken, updateUser)
-
-export default router;
+export const verifyToken = (req, res, next) => {
+  const token = req.cookies.access_token;
+  if (!token) return next(errorHandler(401, 'Unauthorized'));
+  jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
+    if (err) return next(errorHandler(403, 'Forbidden'));
+    req.user = user;
+    next();
+  });
+};
