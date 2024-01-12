237f855e0268878dca6292fa03d9b694e41ae0ff
abdulwasay <abdulwasayhashmi983@gmail.com>
2024-01-12 16:48:09 +0500

    add sign out functionality

--- cleint/src/redux/user/userSlice.js
+++ cleint/src/redux/user/userSlice.js
@@ -44,6 +44,18 @@ const userSlice = createSlice({
       state.error = action.payload;
       state.loading = false;
     },
+    signOutUserStart: (state) => {
+      state.loading = true;
+    },
+    signOutUserSuccess: (state) => {
+      state.currentUser = null;
+      state.loading = false;
+      state.error = null;
+    },
+    signOutUserFailure: (state, action) => {
+      state.error = action.payload;
+      state.loading = false;
+    },
   },
 });
 
@@ -57,6 +69,9 @@ export const {
   deleteUserFailure,
   deleteUserSuccess,
   deleteUserStart,
+  signOutUserFailure,
+  signOutUserSuccess,
+  signOutUserStart,
 } = userSlice.actions;
 
 export default userSlice.reducer;
