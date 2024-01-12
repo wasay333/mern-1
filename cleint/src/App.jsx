414b0802c9aa08a033fbc2479af584cd228b7063
abdulwasay <abdulwasayhashmi983@gmail.com>
2024-01-12 16:11:03 +0500

    made profile page

--- cleint/src/App.jsx
+++ cleint/src/App.jsx
@@ -1,10 +1,7 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Home from './Pages/Home';
-import SignIn from './Pages/SignIn';
-import SignUp from './Pages/Signup';
-import About from './Pages/About';
-import Profile from './Pages/Profile';
-import Header from './Component/Header';
+import About from './pages/About';
+import Profile from './pages/Profile';
+import Header from './components/Header';
+import PrivateRoute from './components/PrivateRoute';
 
 export default function App() {
   return (
@@ -15,7 +12,9 @@ export default function App() {
         <Route path='/sign-in' element={<SignIn />} />
         <Route path='/sign-up' element={<SignUp />} />
         <Route path='/about' element={<About />} />
-        <Route path='/profile' element={<Profile />} />
+        <Route element={<PrivateRoute />}>
+          <Route path='/profile' element={<Profile />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
